14868fd2749a558d433fab9d5170124d7d6f57f5
Espen Klem <espen.klem@gmail.com>
2021-04-28 21:02:00 +0200

    layout to rest of code

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ fs.readdir('./docs-wolaytta', { withFileTypes: true }, (err, files) => {
   }
   files.forEach(file => {
     textFiles.push(file.name)
-    console.log(file.name)
+    // console.log(file.name)
   })
   console.log(textFiles)
+  // trigger streaming function
+  //   a: read a file
+  //   b: process to paragraph array
+  //   c: push to nd-json file
 })

6545741ce9082ca2faff85801ea44f3cd2b03c92
Espen Klem <espen.klem@gmail.com>
2021-04-29 14:14:32 +0200

    working version Cleaning up files a bit Adding some documentation

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # stopword-processing
 Misc tools for processing different text sources, preparing to create stopword lists for different languages.
+
+## create nd-json from Wolaytta text files
+To process with stopword-trainer.
+
+### Usage
+Fork project
+
+```console
+npm install
+```
+
+Copy files to `docs`-folder
+
+```console
+node index.js
+```
+
+Outputs to `wolaytta-words.str`. Use this with `stopword-trainer`.
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
 const wnn = require('words-n-numbers')
-const path = './docs-wolaytta'
-const docs = []
 
-// console.log(textFiles)
+const path = './docs'
+const docs = []
+const docsProcessed = []
+const outputFile = 'wolaytta-words.str'
+let ndJson = ''
+const customRegex = '[\\p{Alpha}\'"7]+'
+const wnnOptions = {
+  regex: customRegex,
+  toLowercase: true
+}
+
 // No triggering streaming function, just do it simple and see if it is enough
 //   a: read directory
 //   b: read all files
@@ -16,16 +24,15 @@ fs.readdir(path, { withFileTypes: true }, (err, files) => {
     console.error(err)
     return
   }
-
   //   b: read all files
   files.forEach(file => {
     const filePath = path + '/' + file.name
     console.log(filePath)
     readFile(path + '/' + file.name)
   })
-
   //   c: process to chunks (extract words and create chunks)
-  processDocs(docs)
+  //   d: write to nd-json file
+  processAndWriteDocs(docs)
 })
 
 function readFile (filePath) {
@@ -34,7 +41,22 @@ function readFile (filePath) {
   docs.push(data)
 }
 
-function processDocs (docs) {
+function processAndWriteDocs (docs) {
   // some wnn-stuff
-  console.log(JSON.stringify(docs))
+  docs.forEach(doc => {
+    docsProcessed.push(wnn.extract(doc, wnnOptions))
+  })
+  console.dir(docsProcessed)
+  // create nd-json, a little hackish
+  docsProcessed.forEach(doc => {
+    ndJson = '{"doc":' + JSON.stringify(doc) + '}\n'
+    fs.writeFileSync(outputFile,
+      ndJson,
+      {
+        encoding: 'utf8',
+        flag: 'a+',
+        mode: 0o666
+      })
+  })
+  console.dir(ndJson)
 }

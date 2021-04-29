3f53bdd863270251cf88d6062d50f6819a8fe964
Espen Klem <espen.klem@gmail.com>
2021-04-29 07:45:17 +0200

    reading files

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const fs = require('fs')
-const textFiles = []
+const path = './docs-wolaytta'
 
-fs.readdir('./docs-wolaytta', { withFileTypes: true }, (err, files) => {
+fs.readdir(path, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.error(err)
     return
   }
   files.forEach(file => {
-    textFiles.push(file.name)
+    let filePath = path + '/' + file.name
+    console.log(filePath)
+    readFile(path + '/' + file.name)
     // console.log(file.name)
   })
-  console.log(textFiles)
-  // trigger streaming function
-  //   a: read a file
-  //   b: process to paragraph array
-  //   c: push to nd-json file
+  // console.log(textFiles)
+  // No triggering streaming function, just do it simple and see if it is enough
+  //   a: read all files
+  //   b: process to chunks (extract words and create chunks)
+  //   c: write to nd-json file
 })
+
+function readFile (filePath) {
+  const data = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' })
+  console.log(data)
+}

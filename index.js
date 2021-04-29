bce727c2e1197f8d8dcfb0ee8bb76c4b716390bf
Espen Klem <espen.klem@gmail.com>
2021-04-29 07:59:34 +0200

    populating docs array

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const fs = require('fs')
+const wnn = require('words-n-numbers')
 const path = './docs-wolaytta'
+let docs = []
 
+// console.log(textFiles)
+// No triggering streaming function, just do it simple and see if it is enough
+//   a: read directory
+//   b: read all files
+//   c: process to chunks (extract words and create chunks)
+//   d: write to nd-json file
+
+//   a: read directory
 fs.readdir(path, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.error(err)
     return
   }
   files.forEach(file => {
-    let filePath = path + '/' + file.name
+    const filePath = path + '/' + file.name
     console.log(filePath)
+
+    //   b: read all files
     readFile(path + '/' + file.name)
-    // console.log(file.name)
   })
-  // console.log(textFiles)
-  // No triggering streaming function, just do it simple and see if it is enough
-  //   a: read all files
-  //   b: process to chunks (extract words and create chunks)
-  //   c: write to nd-json file
+  console.log(JSON.stringify(docs))
 })
 
 function readFile (filePath) {
   const data = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' })
-  console.log(data)
+  // console.log(data)
+  docs.push(data)
+  // need to push to array docs
+}
+
+function processFiles () {
+  // some wnn-stuff
 }
